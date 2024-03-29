78484fd17a58413553b452e883a0dfb725442bd7
Ирина Баранова <irina.baranova14@outlook.com>
2024-03-29 22:56:56 +0300

    Добавьте файлы проекта.

---
+++ CreationModelPlugin/Class1.cs
@@ -0,0 +1,78 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationModelPlugin
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class CreationModel : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            Level level1 = GetLevel(doc, "Уровень 1");
+            Level level2 = GetLevel(doc, "Уровень 2");
+
+
+
+            double width = UnitUtils.ConvertToInternalUnits(10000, UnitTypeId.Millimeters);
+            double depth = UnitUtils.ConvertToInternalUnits(5000, UnitTypeId.Millimeters);
+            double dx = width / 2;
+            double dy = depth  / 2;
+
+            List<XYZ> points = new List<XYZ>();
+            points.Add(new XYZ(-dx, -dy, 0));
+            points.Add(new XYZ(dx, -dy, 0));
+            points.Add(new XYZ(dx, dy, 0));
+            points.Add(new XYZ(-dx, dy, 0));
+            points.Add(new XYZ(-dx, -dy, 0));
+
+            Transaction transaction = new Transaction(doc, "Построекние стены");
+            transaction.Start();
+
+            List<Wall> walls = new List<Wall>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Line line = Line.CreateBound(points[i], points[i + 1]);
+                Wall wall = Wall.Create(doc, line, level1.Id, false);
+                walls.Add(wall);
+                wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(level2.Id);
+            }
+
+
+            transaction.Commit();
+
+
+            return Result.Succeeded;
+
+
+        }
+
+        private static Level GetLevel(Document doc, string name)
+        {
+
+            List<Level> listLevel = new FilteredElementCollector(doc)
+             .OfClass(typeof(Level))
+             .OfType<Level>()
+             .ToList();
+
+            Level level = listLevel
+                 .Where(x => x.Name.Equals(name))
+                 .FirstOrDefault();
+
+            return level;
+        }
+
+
+    }
+}
